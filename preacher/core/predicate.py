9830e5cc6ac4d185aaa5e86a63337285272cc237
ymoch <ymoch.dev@gmail.com>
2019-03-10 11:57:09 +0900

    Refactor to use Hamcrest.

--- preacher/core/predicate.py
+++ preacher/core/predicate.py
@@ -2,8 +2,27 @@
 
 from typing import Any, Optional
 
+import hamcrest
+from hamcrest.core.matcher import Matcher as HamcrestMatcher
+
 from .description import Predicate, VerifiedValue
 from .verification import Verification
+
+
+def hamcrest_predicate(matcher: HamcrestMatcher) -> Predicate:
+    """
+    Make a predicate from a Hamcrest matcher.
+    """
+    def _test(actual: VerifiedValue) -> Verification:
+        try:
+            hamcrest.assert_that(actual, matcher)
+        except AssertionError as error:
+            message = str(error).strip()
+            return Verification(is_valid=False, message=message)
+
+        return Verification(is_valid=True)
+
+    return _test
 
 
 def equal_to(expected: Optional[Any]) -> Predicate:
@@ -18,27 +37,21 @@ def equal_to(expected: Optional[Any]) -> Predicate:
     >>> verification.is_valid
     False
     >>> verification.message
-    'Expected: equal to None, Actual: 0'
+    'Expected: <None>\\n     but: was <0>'
 
     >>> predicate = equal_to(1)
     >>> verification = predicate(0)
     >>> verification.is_valid
     False
     >>> verification.message
-    'Expected: equal to 1, Actual: 0'
+    'Expected: <1>\\n     but: was <0>'
     >>> verification = predicate('1')
     >>> verification.is_valid
     False
     >>> verification.message
-    'Expected: equal to 1, Actual: 1'
+    "Expected: <1>\\n     but: was \'1\'"
     >>> verification = predicate(1)
     >>> verification.is_valid
     True
     """
-    def test(actual: VerifiedValue) -> Verification:
-        is_valid = actual == expected
-        return Verification(
-            is_valid=is_valid,
-            message=f'Expected: equal to {expected}, Actual: {actual}',
-        )
-    return test
+    return hamcrest_predicate(hamcrest.equal_to(expected))
