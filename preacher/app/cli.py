b2c43830720c88479df87d998a7c374318b0b8b4
ymoch <ymoch.dev@gmail.com>
2019-03-13 01:24:41 +0900

    Run with yaml configurations.

--- preacher/app/cli.py
+++ preacher/app/cli.py
@@ -1,30 +1,43 @@
 """Preacher CLI."""
 
 import argparse
+import functools
+import os
 import sys
+
+import yaml
 
 from preacher import __version__ as VERSION
 from preacher.core.compilation import compile_description
+from preacher.core.verification import Status, Verification
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--version', action='version', version=VERSION)
+    parser.add_argument('conf', nargs='+')
 
     return parser.parse_args()
 
 
+def process_by_config(config_path: os.PathLike) -> Verification:
+    with open(config_path) as config_file:
+        config = yaml.load(config_file)
+    description = compile_description(config)
+    data = {'foo': 'bar'}
+    return description(data)
+
+
 def main() -> None:
     """Main."""
-    parse_args()
+    args = parse_args()
+    config_paths = args.conf
 
-    description = compile_description({
-        'jq': '.foo',
-        'it': {
-            'equals_to': 'bar',
-        },
-    })
-    data = {'foo': 'bar'}
-    verification = description(data)
-    if not verification.status.is_succeeded:
+    verifications = (process_by_config(path) for path in config_paths)
+    status = functools.reduce(
+        lambda a, b: a.merge(b.status),
+        verifications,
+        Status.SUCCESS,
+    )
+    if not status.is_succeeded:
         sys.exit(1)
