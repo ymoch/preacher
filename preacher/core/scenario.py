8e01cf72cf5929b658544ecc055fb5493e237b73
ymoch <ymoch.dev@gmail.com>
2019-03-18 00:07:02 +0900

    Add tests.

--- preacher/core/scenario.py
+++ preacher/core/scenario.py
@@ -22,6 +22,49 @@ class ResponseScenarioVerification:
 
 
 class ResponseScenario:
+    """
+    >>> scenario = ResponseScenario(
+    ...     body_descriptions=[],
+    ... )
+    >>> verification = scenario(body='')
+    >>> verification.status.name
+    'SUCCESS'
+
+    >>> from unittest.mock import MagicMock
+    >>> scenario = ResponseScenario(
+    ...     body_descriptions=[MagicMock(return_value=Verification.succeed())],
+    ... )
+    >>> verification = scenario(body='invalid-format')
+    >>> scenario.body_descriptions[0].call_count
+    0
+    >>> verification.status.name
+    'FAILURE'
+    >>> verification.body.status.name
+    'FAILURE'
+    >>> verification.body.message
+    'JSONDecodeError: Expecting value: line 1 column 1 (char 0)'
+
+    >>> from unittest.mock import MagicMock
+    >>> scenario = ResponseScenario(
+    ...     body_descriptions=[
+    ...         MagicMock(return_value=Verification(status=Status.UNSTABLE)),
+    ...         MagicMock(return_value=Verification.succeed()),
+    ...     ],
+    ... )
+    >>> verification = scenario(body='{}')
+    >>> scenario.body_descriptions[0].call_args
+    call({})
+    >>> scenario.body_descriptions[1].call_args
+    call({})
+    >>> verification.status.name
+    'UNSTABLE'
+    >>> verification.body.status.name
+    'UNSTABLE'
+    >>> verification.body.children[0].status.name
+    'UNSTABLE'
+    >>> verification.body.children[1].status.name
+    'SUCCESS'
+    """
     def __init__(
         self: ResponseScenario,
         body_descriptions: List[Description],
@@ -48,6 +91,9 @@ class ResponseScenario:
         return self._body_descriptions
 
     def _verify_body(self: ResponseScenario, body: str) -> Verification:
+        if not self._body_descriptions:
+            return Verification.succeed()
+
         data = json.loads(body)
         verifications = [
             describe(data) for describe in self._body_descriptions
