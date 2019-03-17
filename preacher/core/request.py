d5a345d0c6ad2a678b0dd5cca1ffe3334d5db2a7
ymoch <ymoch.dev@gmail.com>
2019-03-17 22:22:52 +0900

    Add tests.

--- preacher/core/request.py
+++ preacher/core/request.py
@@ -14,6 +14,26 @@ class Response:
 
 
 class Request:
+    """
+    >>> from unittest.mock import MagicMock, patch
+    >>> inner_response = MagicMock(
+    ...     requests.Response,
+    ...     status_code=402,
+    ...     headers={'header-key': 'header-value'},
+    ...     text='text',
+    ... )
+    >>> request = Request(path='/path', params={'key': 'value'})
+    >>> with patch('requests.get', return_value=inner_response) as mock:
+    ...     response = request('base-url')
+    ...     mock.call_args
+    call('base-url/path', params={'key': 'value'})
+    >>> response.status_code
+    402
+    >>> response.headers
+    {'header-key': 'header-value'}
+    >>> response.body
+    'text'
+    """
     def __init__(self, path: str, params: Mapping) -> None:
         self._path = path
         self._params = params
