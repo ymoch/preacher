499193e4c7aabb971ae78bb06dfda8441fe49c28
ymoch <ymoch.dev@gmail.com>
2019-04-14 00:59:55 +0900

    Simplify.

--- preacher/compilation/predicate.py
+++ preacher/compilation/predicate.py
@@ -3,37 +3,8 @@
 from collections.abc import Mapping
 from typing import Union
 
-import hamcrest
-
 from preacher.core.predicate import Predicate, of_hamcrest_matcher
 from .matcher import compile as compile_matcher
-
-
-_VALUE_MATCHER_FUNCTION_MAP = {
-    # For objects.
-    'is': lambda expected: hamcrest.is_(expected),
-    'equals_to': lambda expected: hamcrest.is_(hamcrest.equal_to(expected)),
-    'has_length': lambda expected: hamcrest.has_length(expected),
-    # For numbers.
-    'is_greater_than': (
-        lambda value: hamcrest.is_(hamcrest.greater_than(value))
-    ),
-    'is_greater_than_or_equal_to': (
-        lambda value: hamcrest.is_(hamcrest.greater_than_or_equal_to(value))
-    ),
-    'is_less_than': (
-        lambda value: hamcrest.is_(hamcrest.less_than(value))
-    ),
-    'is_less_than_or_equal_to': (
-        lambda value: hamcrest.is_(hamcrest.less_than_or_equal_to(value))
-    ),
-    # For strings.
-    'contains_string': lambda value: hamcrest.contains_string(value),
-    'starts_with': lambda value: hamcrest.starts_with(value),
-    'ends_with': lambda value: hamcrest.ends_with(value),
-    'matches_regexp': lambda value: hamcrest.matches_regexp(value),
-}
-_PREDICATE_KEYS = frozenset(_VALUE_MATCHER_FUNCTION_MAP.keys())
 
 
 def compile(obj: Union[str, Mapping]) -> Predicate:
