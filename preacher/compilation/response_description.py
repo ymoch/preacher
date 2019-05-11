1fcab1de63b4c2621b111b80d427e2f5f95ec39b
ymoch <ymoch.dev@gmail.com>
2019-05-11 11:39:01 +0900

    Refactor.

--- preacher/compilation/response_description.py
+++ preacher/compilation/response_description.py
@@ -1,5 +1,7 @@
 """Response description compilations."""
 
+from __future__ import annotations
+
 from collections.abc import Mapping
 from typing import Any
 
@@ -13,6 +15,48 @@ from .util import map_on_key
 
 _KEY_STATUS_CODE = 'status_code'
 _KEY_BODY = 'body'
+
+
+class ResponseDescriptionCompiler:
+    def compile(
+        self: ResponseDescriptionCompiler,
+        obj: Mapping,
+    ) -> ResponseDescription:
+        status_code_predicate_objs = obj.get(_KEY_STATUS_CODE, [])
+        if not isinstance(status_code_predicate_objs, list):
+            status_code_predicate_objs = [status_code_predicate_objs]
+        status_code_predicates = list(map_on_key(
+            key=_KEY_STATUS_CODE,
+            func=compile_predicate,
+            items=status_code_predicate_objs,
+        ))
+
+        body_description_objs = obj.get(_KEY_BODY, [])
+        if isinstance(body_description_objs, Mapping):
+            body_description_objs = [body_description_objs]
+        if not isinstance(body_description_objs, list):
+            raise CompilationError(
+                message='ResponseDescription.body must be a list or a mapping',
+                path=[_KEY_BODY],
+            )
+        body_descriptions = list(map_on_key(
+            key=_KEY_BODY,
+            func=self._compile_description,
+            items=body_description_objs,
+        ))
+
+        return ResponseDescription(
+            status_code_predicates=status_code_predicates,
+            body_descriptions=body_descriptions,
+        )
+
+    def _compile_description(
+        self: ResponseDescriptionCompiler,
+        obj: Any,
+    ) -> Description:
+        if not isinstance(obj, Mapping):
+            raise CompilationError('Description must be a mapping')
+        return compile_description(obj)
 
 
 def compile(obj: Mapping) -> ResponseDescription:
@@ -75,36 +119,5 @@ def compile(obj: Mapping) -> ResponseDescription:
     >>> response_description.body_descriptions
     [sentinel.description, sentinel.description]
     """
-    status_code_predicate_objs = obj.get(_KEY_STATUS_CODE, [])
-    if not isinstance(status_code_predicate_objs, list):
-        status_code_predicate_objs = [status_code_predicate_objs]
-    status_code_predicates = list(map_on_key(
-        key=_KEY_STATUS_CODE,
-        func=compile_predicate,
-        items=status_code_predicate_objs,
-    ))
-
-    body_description_objs = obj.get(_KEY_BODY, [])
-    if isinstance(body_description_objs, Mapping):
-        body_description_objs = [body_description_objs]
-    if not isinstance(body_description_objs, list):
-        raise CompilationError(
-            message='ResponseDescription.body must be a list or a mapping',
-            path=[_KEY_BODY],
-        )
-    body_descriptions = list(map_on_key(
-        key=_KEY_BODY,
-        func=_compile_description,
-        items=body_description_objs,
-    ))
-
-    return ResponseDescription(
-        status_code_predicates=status_code_predicates,
-        body_descriptions=body_descriptions,
-    )
-
-
-def _compile_description(obj: Any) -> Description:
-    if not isinstance(obj, Mapping):
-        raise CompilationError('Description must be a mapping')
-    return compile_description(obj)
+    compiler = ResponseDescriptionCompiler()
+    return compiler.compile(obj)
