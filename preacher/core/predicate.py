b3d6a7f591c3953a993f54d17d83b914406f376c
ymoch <ymoch.dev@gmail.com>
2019-05-11 14:26:42 +0900

    Refactor to make predicates serializable.

--- preacher/core/predicate.py
+++ preacher/core/predicate.py
@@ -1,5 +1,7 @@
 """Predicate."""
 
+from __future__ import annotations
+
 from typing import Any
 
 from hamcrest import assert_that
@@ -10,18 +12,35 @@ from .status import Status
 from .verification import Verification
 
 
+class MatcherPredicate:
+    def __init__(self: MatcherPredicate, matcher: Matcher) -> None:
+        self._matcher = matcher
+
+    def __call__(self: MatcherPredicate, actual: Any) -> Verification:
+        try:
+            assert_that(actual, self._matcher)
+        except AssertionError as error:
+            message = str(error).strip()
+            return Verification(status=Status.UNSTABLE, message=message)
+        except Exception as error:
+            return Verification.of_error(error)
+
+        return Verification.succeed()
+
+
 def of_hamcrest_matcher(matcher: Matcher) -> Predicate:
     """
     Make a predicate from a Hamcrest matcher.
 
     >>> from unittest.mock import MagicMock, patch
     >>> matcher = MagicMock(Matcher)
+    >>> predicate = of_hamcrest_matcher(matcher)
+    >>> assert isinstance(predicate, MatcherPredicate)
 
     >>> with patch(
     ...     f'{__name__}.assert_that',
     ...     side_effect=RuntimeError('message')
     ... ) as assert_that:
-    ...     predicate = of_hamcrest_matcher(matcher)
     ...     verification = predicate(0)
     ...     assert_that.assert_called_with(0, matcher)
     >>> verification.status
@@ -33,7 +52,6 @@ def of_hamcrest_matcher(matcher: Matcher) -> Predicate:
     ...     f'{__name__}.assert_that',
     ...     side_effect=AssertionError(' message\\n')
     ... ) as assert_that:
-    ...     predicate = of_hamcrest_matcher(matcher)
     ...     verification = predicate(0)
     ...     assert_that.assert_called_with(0, matcher)
     >>> verification.status
@@ -42,21 +60,9 @@ def of_hamcrest_matcher(matcher: Matcher) -> Predicate:
     'message'
 
     >>> with patch(f'{__name__}.assert_that') as assert_that:
-    ...     predicate = of_hamcrest_matcher(matcher)
     ...     verification = predicate(1)
     ...     assert_that.assert_called_with(1, matcher)
     >>> verification.status
     SUCCESS
     """
-    def _test(actual: Any) -> Verification:
-        try:
-            assert_that(actual, matcher)
-        except AssertionError as error:
-            message = str(error).strip()
-            return Verification(status=Status.UNSTABLE, message=message)
-        except Exception as error:
-            return Verification.of_error(error)
-
-        return Verification.succeed()
-
-    return _test
+    return MatcherPredicate(matcher)
