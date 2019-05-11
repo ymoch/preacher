4a0de4554b295957555bc273dda2e569b4db5003
ymoch <ymoch.dev@gmail.com>
2019-05-11 12:05:08 +0900

    Refactor.

--- preacher/compilation/predicate.py
+++ preacher/compilation/predicate.py
@@ -1,9 +1,27 @@
 """Predicate compilation."""
 
+from __future__ import annotations
 from typing import Any
 
 from preacher.core.predicate import Predicate, of_hamcrest_matcher
 from .matcher import compile as compile_matcher
+
+
+class PredicateCompiler:
+    """
+    >>> from unittest.mock import patch, sentinel
+
+    >>> compiler = PredicateCompiler()
+    >>> with patch(
+    ...     f'{__name__}.compile_matcher',
+    ...     return_value=sentinel.matcher
+    ... ) as matcher_mock:
+    ...     predicate = compiler.compile('matcher')
+    ...     matcher_mock.assert_called_with('matcher')
+    """
+    def compile(self: PredicateCompiler, obj: Any) -> Predicate:
+        matcher = compile_matcher(obj)
+        return of_hamcrest_matcher(matcher)
 
 
 def compile(obj: Any) -> Predicate:
@@ -17,5 +35,5 @@ def compile(obj: Any) -> Predicate:
     ...     predicate = compile('matcher')
     ...     matcher_mock.assert_called_with('matcher')
     """
-    matcher = compile_matcher(obj)
-    return of_hamcrest_matcher(matcher)
+    compiler = PredicateCompiler()
+    return compiler.compile(obj)
