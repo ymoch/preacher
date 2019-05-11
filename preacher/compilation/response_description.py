4c100b4160d936a768f06f3a039a7114e504838f
ymoch <ymoch.dev@gmail.com>
2019-05-11 11:53:24 +0900

    Remove unused functions.

--- preacher/compilation/response_description.py
+++ preacher/compilation/response_description.py
@@ -18,6 +18,70 @@ _KEY_BODY = 'body'
 
 
 class ResponseDescriptionCompiler:
+    """
+    >>> from unittest.mock import call, patch, sentinel
+    >>> predicate_patch = patch(
+    ...     f'{__name__}.compile_predicate',
+    ...     return_value=sentinel.predicate,
+    ... )
+    >>> description_patch = patch(
+    ...     f'{__name__}.compile_description',
+    ...     return_value=sentinel.description,
+    ... )
+
+    >>> compiler = ResponseDescriptionCompiler()
+    >>> with predicate_patch as predicate_mock, \\
+    ...      description_patch as description_mock:
+    ...     response_description = compiler.compile({})
+    ...     predicate_mock.assert_not_called()
+    ...     description_mock.assert_not_called()
+    >>> response_description.status_code_predicates
+    []
+    >>> response_description.body_descriptions
+    []
+
+    >>> compiler = ResponseDescriptionCompiler()
+    >>> compiler.compile({'body': 'str'})
+    Traceback (most recent call last):
+        ...
+    preacher.compilation.error.CompilationError: ...: body
+
+    >>> compiler = ResponseDescriptionCompiler()
+    >>> compiler.compile({'body': ['str']})
+    Traceback (most recent call last):
+        ...
+    preacher.compilation.error.CompilationError: Description ...: body[0]
+
+    >>> compiler = ResponseDescriptionCompiler()
+    >>> with predicate_patch as predicate_mock, \\
+    ...      description_patch as description_mock:
+    ...     response_description = compiler.compile({
+    ...         'status_code': 402,
+    ...         'body': {'key1': 'value1'}}
+    ...     )
+    ...     predicate_mock.assert_called_once_with(402)
+    ...     description_mock.assert_called_once_with({'key1': 'value1'})
+    >>> response_description.body_descriptions
+    [sentinel.description]
+
+    >>> compiler = ResponseDescriptionCompiler()
+    >>> with predicate_patch as predicate_mock, \\
+    ...      description_patch as description_mock:
+    ...     response_description = compiler.compile({
+    ...         'status_code': [{'be_greater_than': 0}, {'be_less_than': 400}],
+    ...         'body': [{'key1': 'value1'}, {'key2': 'value2'}],
+    ...     })
+    ...     predicate_mock.assert_has_calls([
+    ...         call({'be_greater_than': 0}),
+    ...         call({'be_less_than': 400}),
+    ...     ])
+    ...     description_mock.assert_has_calls([
+    ...         call({'key1': 'value1'}),
+    ...         call({'key2': 'value2'}),
+    ...     ])
+    >>> response_description.body_descriptions
+    [sentinel.description, sentinel.description]
+    """
     def compile(
         self: ResponseDescriptionCompiler,
         obj: Mapping,
@@ -57,67 +121,3 @@ class ResponseDescriptionCompiler:
         if not isinstance(obj, Mapping):
             raise CompilationError('Description must be a mapping')
         return compile_description(obj)
-
-
-def compile(obj: Mapping) -> ResponseDescription:
-    """
-    >>> from unittest.mock import call, patch, sentinel
-    >>> predicate_patch = patch(
-    ...     f'{__name__}.compile_predicate',
-    ...     return_value=sentinel.predicate,
-    ... )
-    >>> description_patch = patch(
-    ...     f'{__name__}.compile_description',
-    ...     return_value=sentinel.description,
-    ... )
-
-    >>> with predicate_patch as predicate_mock, \\
-    ...      description_patch as description_mock:
-    ...     response_description = compile({})
-    ...     predicate_mock.assert_not_called()
-    ...     description_mock.assert_not_called()
-    >>> response_description.status_code_predicates
-    []
-    >>> response_description.body_descriptions
-    []
-
-    >>> compile({'body': 'str'})
-    Traceback (most recent call last):
-        ...
-    preacher.compilation.error.CompilationError: ...: body
-
-    >>> compile({'body': ['str']})
-    Traceback (most recent call last):
-        ...
-    preacher.compilation.error.CompilationError: Description ...: body[0]
-
-    >>> with predicate_patch as predicate_mock, \\
-    ...      description_patch as description_mock:
-    ...     response_description = compile({
-    ...         'status_code': 402,
-    ...         'body': {'key1': 'value1'}}
-    ...     )
-    ...     predicate_mock.assert_called_once_with(402)
-    ...     description_mock.assert_called_once_with({'key1': 'value1'})
-    >>> response_description.body_descriptions
-    [sentinel.description]
-
-    >>> with predicate_patch as predicate_mock, \\
-    ...      description_patch as description_mock:
-    ...     response_description = compile({
-    ...         'status_code': [{'be_greater_than': 0}, {'be_less_than': 400}],
-    ...         'body': [{'key1': 'value1'}, {'key2': 'value2'}],
-    ...     })
-    ...     predicate_mock.assert_has_calls([
-    ...         call({'be_greater_than': 0}),
-    ...         call({'be_less_than': 400}),
-    ...     ])
-    ...     description_mock.assert_has_calls([
-    ...         call({'key1': 'value1'}),
-    ...         call({'key2': 'value2'}),
-    ...     ])
-    >>> response_description.body_descriptions
-    [sentinel.description, sentinel.description]
-    """
-    compiler = ResponseDescriptionCompiler()
-    return compiler.compile(obj)
