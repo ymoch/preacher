518fec4c8b39152e0ae9bf4ba3abf748393e13f9
ymoch <ymoch.dev@gmail.com>
2019-05-26 17:22:42 +0900

    Refactor.

--- preacher/compilation/request.py
+++ preacher/compilation/request.py
@@ -3,14 +3,47 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
-from typing import Union
+from dataclasses import dataclass
+from typing import Optional, Union
 
 from preacher.core.request import Request
 from .error import CompilationError
+from .util import or_default
 
 
 _KEY_PATH = 'path'
 _KEY_PARAMS = 'params'
+
+
+@dataclass
+class _Compiled:
+    path: Optional[str] = None
+    params: Optional[Mapping] = None
+
+    def to_request(
+        self: _Compiled,
+        default_path: str,
+        default_params: Mapping,
+    ) -> Request:
+        return Request(
+            path=or_default(self.path, default_path),
+            params=or_default(self.params, default_params),
+        )
+
+
+def _compile(obj: Union[Mapping, str]) -> _Compiled:
+    if isinstance(obj, str):
+        return _compile({_KEY_PATH: obj})
+
+    path = obj.get(_KEY_PATH)
+    if path is not None and not isinstance(path, str):
+        raise CompilationError('Must be a string', path=[_KEY_PATH])
+
+    params = obj.get(_KEY_PARAMS)
+    if params is not None and not isinstance(params, Mapping):
+        raise CompilationError('Must be a mapping', path=[_KEY_PARAMS])
+
+    return _Compiled(path=path, params=params)
 
 
 class RequestCompiler:
@@ -19,23 +52,23 @@ class RequestCompiler:
     >>> RequestCompiler({'path': {'key': 'value'}})
     Traceback (most recent call last):
         ...
-    preacher.compilation.error.CompilationError: Request.path ...: path
+    preacher.compilation.error.CompilationError: ...: path
     >>> compiler = RequestCompiler()
     >>> compiler.compile({'path': {'key': 'value'}})
     Traceback (most recent call last):
         ...
-    preacher.compilation.error.CompilationError: Request.path ...: path
+    preacher.compilation.error.CompilationError: ...: path
 
     When given not a mapping parameters, then raises a compilation error.
     >>> RequestCompiler({'params': ''})
     Traceback (most recent call last):
         ...
-    preacher.compilation.error.CompilationError: Request.params ...: params
+    preacher.compilation.error.CompilationError: ...: params
     >>> compiler = RequestCompiler()
     >>> compiler.compile({'params': ''})
     Traceback (most recent call last):
         ...
-    preacher.compilation.error.CompilationError: Request.params ...: params
+    preacher.compilation.error.CompilationError: ...: params
 
     When given an empty mapping, then returns the dafault mapping..
     >>> compiler = RequestCompiler()
@@ -84,36 +117,23 @@ class RequestCompiler:
         self: RequestCompiler,
         defaults: Union[Mapping, str, None] = None,
     ) -> None:
-        self._default_path: str = ''
-        self._default_params: Mapping = {}
+        self._path: str = ''
+        self._params: Mapping = {}
 
         if defaults:
             self.set_defaults(defaults)
 
     def set_defaults(
         self: RequestCompiler,
-        defaults: Union[Mapping, str],
+        obj: Union[Mapping, str],
     ) -> None:
-        default_request = RequestCompiler().compile(defaults)
-        self._default_path = default_request.path
-        self._default_params = default_request.params
+        compiled = _compile(obj)
+        self._path = or_default(compiled.path, self._path)
+        self._params = or_default(compiled.params, self._params)
 
     def compile(self: RequestCompiler, obj: Union[Mapping, str]) -> Request:
-        if isinstance(obj, str):
-            return self.compile({_KEY_PATH: obj})
-
-        path = obj.get(_KEY_PATH, self._default_path)
-        if not isinstance(path, str):
-            raise CompilationError(
-                message=f'Request.{_KEY_PATH} must be a string',
-                path=[_KEY_PATH],
-            )
-
-        params = obj.get(_KEY_PARAMS, self._default_params)
-        if not isinstance(params, Mapping):
-            raise CompilationError(
-                message=f'Request.{_KEY_PARAMS} must be a mapping',
-                path=[_KEY_PARAMS],
-            )
-
-        return Request(path=path, params=params)
+        compiled = _compile(obj)
+        return compiled.to_request(
+            default_path=self._path,
+            default_params=self._params,
+        )
