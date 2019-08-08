121efc3b0fb5980f3dae578eca071e971ee5d9cb
ymoch <ymoch.dev@gmail.com>
2019-08-09 00:39:02 +0900

    Simplify.

---
+++ tests/core/test_request.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock, patch
+
+from requests import Response
+
+from preacher.core.request import Request
+
+
+@patch('requests.get')
+def test_request(requests_get):
+    inner_response = MagicMock(
+        spec=Response,
+        status_code=402,
+        headers={'header-key': 'header-value'},
+        text='text',
+    )
+    requests_get.return_value = inner_response
+
+    request = Request(path='/path', params={'key': 'value'})
+    assert request.path == '/path'
+    assert request.params == {'key': 'value'}
+
+    response = request('base-url')
+    assert response.status_code == 402
+    assert response.headers == {'header-key': 'header-value'}
+    assert response.body == 'text'
+
+    args, kwargs = requests_get.call_args
+    assert args == ('base-url/path',)
+    assert kwargs['headers']['User-Agent'].startswith('Preacher')
