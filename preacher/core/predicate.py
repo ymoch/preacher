0c9812d90bce610030e0669ff9a6bf61ef6ca290
ymoch <ymoch.dev@gmail.com>
2019-08-27 15:32:34 +0900

    Add dynamic predicates.

--- preacher/core/predicate.py
+++ preacher/core/predicate.py
@@ -1,6 +1,6 @@
 """Predicate."""
 
-from typing import Any
+from typing import Any, Callable
 
 from hamcrest import assert_that
 from hamcrest.core.matcher import Matcher
@@ -15,7 +15,7 @@ class MatcherPredicate:
     def __init__(self, matcher: Matcher):
         self._matcher = matcher
 
-    def __call__(self, actual: Any, **kwargs: Any) -> Verification:
+    def __call__(self, actual: Any, *args: Any, **kwargs: Any) -> Verification:
         try:
             assert_that(actual, self._matcher)
         except AssertionError as error:
@@ -25,3 +25,18 @@ class MatcherPredicate:
             return Verification.of_error(error)
 
         return Verification.succeed()
+
+
+class DynamicMatcherPredicate:
+
+    def __init__(self, matcher_factory: Callable):
+        self._matcher_factory = matcher_factory
+
+    def __call__(self, actual: Any, *args: Any, **kwargs: Any) -> Verification:
+        try:
+            matcher = self._matcher_factory(*args, **kwargs)
+        except Exception as error:
+            return Verification.of_error(error)
+
+        predicate = MatcherPredicate(matcher)
+        return predicate(actual, *args, **kwargs)
