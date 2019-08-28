a23788b34a6f525b172b80aa282cb62d0afa33dc
ymoch <ymoch.dev@gmail.com>
2019-08-28 16:34:06 +0900

    Parse ISO datetime strings.

--- preacher/core/util.py
+++ preacher/core/util.py
@@ -8,6 +8,15 @@ def now() -> datetime.datetime:
     return datetime.datetime.now(_system_timezone())
 
 
+def parse_datetime(value: str) -> datetime.datetime:
+    """
+    Only takes ISO 6801 expanded format.
+    """
+    if value.endswith('Z'):
+        value = value[:-1] + '+00:00'
+    return datetime.datetime.fromisoformat(value)
+
+
 def _system_timezone() -> datetime.timezone:
     localtime = time.localtime()
     return datetime.timezone(
--- tests/core/test_util.py
+++ tests/core/test_util.py
@@ -1,7 +1,9 @@
 import time
 from unittest.mock import MagicMock, patch
 
-from preacher.core.util import now
+from pytest import raises
+
+from preacher.core.util import now, parse_datetime
 
 
 @patch('time.localtime')
@@ -26,3 +28,33 @@ def test_now_pdt(localtime):
     current = now()
     assert current.tzname() == 'PDT'
     assert current.utcoffset().total_seconds() == -28800
+
+
+def test_parse_date_given_invalid_format():
+    with raises(ValueError) as error_info:
+        parse_datetime('XXX')
+    assert str(error_info.value).endswith('XXX')
+
+
+def test_parse_datetime_given_iso_expanded_format():
+    dt = parse_datetime('2019-08-28T02:45:43.477123-09:00')
+    assert dt.year == 2019
+    assert dt.month == 8
+    assert dt.day == 28
+    assert dt.hour == 2
+    assert dt.minute == 45
+    assert dt.second == 43
+    assert dt.microsecond == 477123
+    assert dt.utcoffset().total_seconds() == -9 * 60 * 60
+
+
+def test_parse_datetime_given_iso_expanded_format_ends_with_z():
+    dt = parse_datetime('2019-08-28T02:45:43.477Z')
+    assert dt.year == 2019
+    assert dt.month == 8
+    assert dt.day == 28
+    assert dt.hour == 2
+    assert dt.minute == 45
+    assert dt.second == 43
+    assert dt.microsecond == 477000
+    assert dt.utcoffset().total_seconds() == 0
