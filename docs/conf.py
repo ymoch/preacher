d54388368401826b60c57bcd5c14c4f6d7654a22
ymoch <ymoch.dev@gmail.com>
2019-10-20 18:44:18 +0900

    Fix.

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ _PROJECT_ROOT_PATH = os.path.abspath(os.path.join(_DOC_ROOT_PATH, '..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Preacher'
+# noinspection PyShadowingBuiltins
 copyright = '2019, Yu Mochizuki'
 author = 'Yu Mochizuki'
 
