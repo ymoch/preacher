400fa58076b5679bbbaa21167cb8ed60dda84a39
ymoch <ymoch.dev@gmail.com>
2019-11-04 14:33:45 +0900

    Fix errors.

--- preacher/app/cli/main.py
+++ preacher/app/cli/main.py
@@ -6,11 +6,11 @@ import sys
 from concurrent.futures import ThreadPoolExecutor
 
 from preacher.app.application import Application
+from preacher.app.cli.logging import ColoredFormatter
+from preacher.app.cli.option import parse_args
 from preacher.app.listener.logging import LoggingListener
 from preacher.app.listener.merging import MergingListener
 from preacher.app.listener.report import ReportingListener
-from .logging import ColoredFormatter
-from .option import parse_args
 
 
 FORMATTER = ColoredFormatter()
