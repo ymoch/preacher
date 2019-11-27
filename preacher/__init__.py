3a7cb438b063a89ad8ba7da6259ff18bd35b5c85
ymoch <ymoch.dev@gmail.com>
2019-11-28 01:39:58 +0900

    Remove unused codes.

--- preacher/__init__.py
+++ preacher/__init__.py
@@ -1,13 +1,3 @@
 """Preacher: A Web API Verification Tool."""
 
 __version__ = '0.11.7'
-__author__ = 'Yu Mochizuki'
-__author_email__ = 'ymoch.dev@gmail.com'
-
-
-def __dummy__() -> None:
-    """
-    Dummy function so that tests cover this module.
-    >>> __dummy__()
-    """
-    pass
