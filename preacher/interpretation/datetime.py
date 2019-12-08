672fa39ea57d8bf9658a5632eea1ba279f0d59f1
ymoch <ymoch.dev@gmail.com>
2019-12-08 17:59:57 +0900

    Extends datetime matchers.

--- preacher/interpretation/datetime.py
+++ preacher/interpretation/datetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any, Optional
 
 from preacher.core.datetime import now
@@ -6,6 +6,10 @@ from .timedelta import interpret_timedelta
 
 
 def interpret_datetime(value: Optional[Any], **kwargs) -> datetime:
+    if isinstance(value, datetime):
+        if not value.tzinfo:
+            return value.astimezone(timezone.utc)
+        return value
     delta = interpret_timedelta(value)
     origin = kwargs.get('request_datetime') or now()
     return origin + delta
