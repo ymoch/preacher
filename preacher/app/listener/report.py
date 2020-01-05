bed81d3014a60120413ce3cfc0a98cfeed80459e
ymoch <ymoch.dev@gmail.com>
2020-01-05 15:25:51 +0900

    Refactor.

--- preacher/app/listener/report.py
+++ preacher/app/listener/report.py
@@ -1,45 +1,24 @@
 from __future__ import annotations
 
-import os
 from typing import List
-
-import jinja2
 
 from preacher.core.request import Response
 from preacher.core.scenario import ScenarioResult
+from preacher.report.html import HtmlReporter
 from . import Listener
 
 
 class ReportingListener(Listener):
 
     def __init__(self, path):
-        self._path = path
-        self._responses_path = os.path.join(self._path, 'responses')
+        self._reporter = HtmlReporter(path)
         self._results: List[ScenarioResult] = []
 
-        env = jinja2.Environment(
-            loader=jinja2.PackageLoader('preacher', 'resources/html'),
-            autoescape=jinja2.select_autoescape(['html', 'xml'])
-        )
-        self._index_template = env.get_template('index.html')
-        self._response_view_template = env.get_template('response-view.html')
-
-        self._initialize()
-
-    def _initialize(self) -> None:
-        os.makedirs(self._path, exist_ok=True)
-        os.makedirs(self._responses_path, exist_ok=True)
-
     def on_response(self, response: Response) -> None:
-        name = f'{response.id}.html'
-        path = os.path.join(self._responses_path, name)
-        with open(path, 'w') as f:
-            self._response_view_template.stream(response=response).dump(f)
+        self._reporter.export_response(response)
 
     def on_scenario(self, result: ScenarioResult) -> None:
         self._results.append(result)
 
     def on_end(self) -> None:
-        html_path = os.path.join(self._path, 'index.html')
-        with open(html_path, 'w') as f:
-            self._index_template.stream(scenarios=self._results).dump(f)
+        self._reporter.export_results(self._results)
---
+++ preacher/report/html.py
@@ -0,0 +1,38 @@
+import os
+from typing import Iterable
+
+import jinja2
+
+from preacher.core.request import Response
+from preacher.core.scenario import ScenarioResult
+
+
+class HtmlReporter:
+
+    def __init__(self, path):
+        self._path = path
+        self._responses_path = os.path.join(self._path, 'responses')
+
+        env = jinja2.Environment(
+            loader=jinja2.PackageLoader('preacher', 'resources/report/html'),
+            autoescape=jinja2.select_autoescape(['html', 'xml']),
+        )
+        self._index_template = env.get_template('index.html')
+        self._response_view_template = env.get_template('response-view.html')
+
+        self._initialize()
+
+    def _initialize(self) -> None:
+        os.makedirs(self._path, exist_ok=True)
+        os.makedirs(self._responses_path, exist_ok=True)
+
+    def export_response(self, response: Response) -> None:
+        name = f'{response.id}.html'
+        path = os.path.join(self._responses_path, name)
+        with open(path, 'w') as f:
+            self._response_view_template.stream(response=response).dump(f)
+
+    def export_results(self, results: Iterable[ScenarioResult]) -> None:
+        html_path = os.path.join(self._path, 'index.html')
+        with open(html_path, 'w') as f:
+            self._index_template.stream(scenarios=results).dump(f)
