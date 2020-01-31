4e3c3a37d688216093a4947a8306b384fd5223a8
ymoch <ymoch.dev@gmail.com>
2020-02-01 01:28:47 +0900

    Refactor.

--- tests/compilation/test_matcher_compile.py
+++ tests/compilation/test_matcher_compile.py
@@ -6,7 +6,8 @@ from preacher.compilation.error import CompilationError
 from preacher.compilation.matcher import compile
 from preacher.core.interpretation.datetime import interpret_datetime
 from preacher.core.hamcrest import after, before
-from preacher.core.scenario import Status, match
+from preacher.core.scenario import Status
+from preacher.core.scenario.matcher import match
 
 PACKAGE = 'preacher.compilation.matcher'
 
