8df2719d40bea13d647d0609bad43d3238ba556d
ymoch <ymoch.dev@gmail.com>
2020-06-12 01:08:30 +0900

    Simplify examples.

--- examples/server.py
+++ examples/server.py
@@ -1,37 +1,31 @@
-import os
 import zlib
 import time
 from datetime import datetime, timedelta, timezone
 
-import responder
+from flask import Flask, jsonify, make_response, render_template, request
 
 
-api = responder.API(
-    static_dir=os.path.join(os.path.dirname(__file__), 'static'),
-    templates_dir=os.path.join(os.path.dirname(__file__), 'templates'),
-)
+api = Flask(__name__,)
 
 
 @api.route('/header')
-def header(req, res) -> None:
-    res.media = {key: value for (key, value) in req.headers.items()}
+def header():
+    return jsonify(dict(request.headers.items()))
 
 
 @api.route('/params')
-def query_params(req, res) -> None:
-    res.media = {key: value for (key, value) in req.params.items()}
-    print(res.media)
+def query_params():
+    return jsonify(dict(request.args.lists()))
 
 
 @api.route('/error/404')
-def not_found(_req, res) -> None:
-    res.status_code = api.status_codes.HTTP_404
-    res.media = {'message': 'not found'}
+def not_found():
+    return jsonify({'message': 'not found'}), 404
 
 
 @api.route('/json')
-def json(_req, res) -> None:
-    res.media = {
+def json():
+    return jsonify({
         'foo': 'bar',
         'empty_string': '',
         'empty_list': [],
@@ -41,36 +35,43 @@ def json(_req, res) -> None:
             {},
             {'x': '2'},
         ],
-    }
+    })
 
 
 @api.route('/xml')
-def xml(_req, res) -> None:
+def xml():
+    res = make_response(render_template('sample.xml'))
     res.headers['content-type'] = 'application/xml'
-    res.content = api.template('sample.xml')
+    return res
 
 
-@api.route('/later/{seconds}')
-def later(_req, res, *, seconds) -> None:
+@api.route('/later/<int:seconds>')
+def later(seconds):
     dt = datetime.now(timezone.utc) + timedelta(seconds=int(seconds))
-    res.media = {'now': dt.isoformat()}
+    return jsonify({'now': dt.isoformat()})
 
 
 @api.route('/text')
-def text(_req, res) -> None:
-    res.text = 'text'
+def text():
+    res = make_response('text')
+    res.headers['content-type'] = 'text/plain'
+    return res
 
 
 @api.route('/binary')
-def binary(_req, res) -> None:
+def binary():
+    res = make_response(zlib.compress(b'text'))
     res.headers['content-type'] = 'application/octet-stream'
-    res.content = zlib.compress(b'text')
+    return res
 
 
-@api.route('/sleep/{seconds}')
-def sleep(_req, res, *, seconds) -> None:
+@api.route('/sleep/<int:seconds>')
+def sleep(seconds):
     time.sleep(float(seconds))
-    res.media = "OK"
+
+    res = make_response('OK')
+    res.headers['content-type'] = 'text/plain'
+    return res
 
 
 def main() -> None:
