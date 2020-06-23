3180e57d77e0519862bb9d0389371486180efdcb
ymoch <ymoch.dev@gmail.com>
2020-06-24 01:37:05 +0900

    Simplify.

--- preacher/core/functional.py
+++ preacher/core/functional.py
@@ -8,18 +8,5 @@ T = TypeVar('T')
 
 
 def identify(value: T, *_args, **_kwargs) -> T:
-    """
-    Returns the first argument.
-    >>> identify(None)
-    >>> identify(1)
-    1
-    >>> identify('a')
-    'a'
-    >>> identify([1, 2, 3])
-    [1, 2, 3]
-
-    Ignores extra arguments.
-    >>> identify(1, 2, key='value')
-    1
-    """
+    """Returns the first argument, ignoring extra arguments."""
     return value
