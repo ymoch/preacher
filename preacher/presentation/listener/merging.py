210add6786d24faa25b616e5600aa8e5357803ec
ymoch <ymoch.dev@gmail.com>
2020-06-25 00:36:38 +0900

    Restructure.

--- preacher/presentation/listener/merging.py
+++ preacher/presentation/listener/merging.py
@@ -12,7 +12,7 @@ class MergingListener(Listener):
     def __init__(self):
         self._listeners: List[Listener] = []
 
-    def append(self, listener: Listener):
+    def append(self, listener: Listener) -> None:
         self._listeners.append(listener)
 
     def on_response(self, response: Response) -> None:
@@ -23,6 +23,6 @@ class MergingListener(Listener):
         for listener in self._listeners:
             listener.on_scenario(result)
 
-    def on_end(self):
+    def on_end(self) -> None:
         for listener in self._listeners:
             listener.on_end()
