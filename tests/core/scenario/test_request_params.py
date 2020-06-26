18eb0a661c74eb3a6863518419ed3a1be47af6be
ymoch <ymoch.dev@gmail.com>
2020-06-27 01:57:38 +0900

    Add tests.

---
+++ tests/core/scenario/test_request_params.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timezone
+from unittest.mock import MagicMock, sentinel
+
+from preacher.core.interpretation.value import Value
+from preacher.core.scenario.request_params import resolve_params
+
+
+def test_resolve_params_given_a_string():
+    resolved = resolve_params('foo=bar')
+    assert resolved == 'foo=bar'
+
+
+def test_resolve_params_given_a_mapping():
+    value = MagicMock(Value)
+    value.apply_context.return_value = sentinel.resolved_value
+
+    params = {
+        'none': None,
+        'false': False,
+        'true': True,
+        'list': [
+            None,
+            1,
+            1.2,
+            'str',
+            datetime(2020, 1, 23, 12, 34, 56, tzinfo=timezone.utc),
+            value,
+        ]
+    }
+    resolved = resolve_params(params, foo=sentinel.foo)
+
+    assert resolved['none'] is None
+    assert resolved['false'] == 'false'
+    assert resolved['true'] == 'true'
+    assert resolved['list'] == [
+        None,
+        '1',
+        '1.2',
+        'str',
+        '2020-01-23T12:34:56+00:00',
+        'sentinel.resolved_value',
+    ]
+
+    value.apply_context.assert_called_once_with(foo=sentinel.foo)
