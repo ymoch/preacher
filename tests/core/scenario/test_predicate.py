354f0505c79316a51cffda2ec8ba184823a3a4fb
ymoch <ymoch.dev@gmail.com>
2020-06-29 00:24:23 +0900

    Refactor tests.

--- tests/core/scenario/test_predicate.py
+++ tests/core/scenario/test_predicate.py
@@ -1,15 +1,15 @@
 from typing import Optional, Any
-from unittest.mock import patch, sentinel
+from unittest.mock import sentinel
 
 from pytest import raises
 
 from preacher.core.scenario.predicate import MatcherPredicate, Predicate
 from preacher.core.scenario.verification import Verification
 
-PACKAGE = 'preacher.core.scenario.predicate'
+PKG = 'preacher.core.scenario.predicate'
 
 
-def test_predicate():
+def test_predicate_interface():
     class IncompletePredicate(Predicate):
         def verify(self, actual: Optional[Any], **kwargs) -> Verification:
             return super().verify(actual, **kwargs)
@@ -18,8 +18,9 @@ def test_predicate():
         IncompletePredicate().verify(sentinel.actual, key='value')
 
 
-@patch(f'{PACKAGE}.match', return_value=sentinel.verification)
-def test_matcher_predicate(match):
+def test_matcher_predicate(mocker):
+    match = mocker.patch(f'{PKG}.match', return_value=sentinel.verification)
+
     predicate = MatcherPredicate(sentinel.matcher)
     verification = predicate.verify(sentinel.actual, key='value')
 
