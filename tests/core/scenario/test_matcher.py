b9f72a1b45d2ef5b52293217dda93a198acd086d
ymoch <ymoch.dev@gmail.com>
2020-06-28 19:58:41 +0900

    Refactor tests.

--- tests/core/scenario/test_matcher.py
+++ tests/core/scenario/test_matcher.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch, sentinel
+from unittest.mock import Mock, NonCallableMock, sentinel
 
 from hamcrest.core.matcher import Matcher as HamcrestMatcher
 from pytest import fixture, raises
@@ -14,17 +14,17 @@ from preacher.core.scenario.matcher import (
 )
 from preacher.core.scenario.status import Status
 
-PACKAGE = 'preacher.core.scenario.matcher'
+PKG = 'preacher.core.scenario.matcher'
 
 
 @fixture
 def hamcrest_factory():
-    return MagicMock(return_value=sentinel.hamcrest)
+    return Mock(return_value=sentinel.hamcrest)
 
 
 @fixture
 def matcher():
-    matcher = MagicMock(Matcher)
+    matcher = NonCallableMock(Matcher)
     matcher.to_hamcrest.return_value = sentinel.hamcrest
     return matcher
 
@@ -45,7 +45,7 @@ def test_static_matcher():
 
 
 def test_value_matcher(hamcrest_factory):
-    value = MagicMock(Value)
+    value = NonCallableMock(Value)
     value.apply_context.return_value = sentinel.value_in_context
 
     matcher = ValueMatcher(hamcrest_factory, value)
@@ -58,10 +58,10 @@ def test_value_matcher(hamcrest_factory):
 
 def test_recursive_matcher(hamcrest_factory):
     inner_matchers = [
-        MagicMock(Matcher, to_hamcrest=MagicMock(
+        NonCallableMock(Matcher, to_hamcrest=Mock(
             return_value=sentinel.inner_hamcrest_0
         )),
-        MagicMock(Matcher, to_hamcrest=MagicMock(
+        NonCallableMock(Matcher, to_hamcrest=Mock(
             return_value=sentinel.inner_hamcrest_1
         )),
     ]
@@ -71,7 +71,6 @@ def test_recursive_matcher(hamcrest_factory):
     assert hamcrest == sentinel.hamcrest
     for inner_matcher in inner_matchers:
         inner_matcher.to_hamcrest.assert_called_once_with(key='value')
-        inner_matcher.to_hamcrest.assert_called_once_with(key='value')
     hamcrest_factory.assert_called_once_with(
         sentinel.inner_hamcrest_0,
         sentinel.inner_hamcrest_1,
@@ -79,7 +78,7 @@ def test_recursive_matcher(hamcrest_factory):
 
 
 def test_match_when_an_interpretation_error_occurs():
-    matcher = MagicMock(Matcher)
+    matcher = NonCallableMock(Matcher)
     matcher.to_hamcrest.side_effect = InterpretationError('interpretation')
     verification = match(matcher, sentinel.actual, k='v')
 
@@ -88,29 +87,31 @@ def test_match_when_an_interpretation_error_occurs():
     matcher.to_hamcrest.assert_called_once_with(k='v')
 
 
-@patch(f'{PACKAGE}.assert_that', side_effect=RuntimeError('message'))
-def test_match_when_an_error_occurs_on_assertion(assert_that, matcher):
-    verification = match(matcher, sentinel.actual, key='value')
+def test_match_when_an_error_occurs_on_assertion(matcher, mocker):
+    assert_that = mocker.patch(f'{PKG}.assert_that')
+    assert_that.side_effect = RuntimeError('message')
 
+    verification = match(matcher, sentinel.actual, key='value')
     assert verification.status == Status.FAILURE
     assert verification.message == 'RuntimeError: message'
     matcher.to_hamcrest.assert_called_once_with(key='value')
     assert_that.assert_called_once_with(sentinel.actual, sentinel.hamcrest)
 
 
-@patch(f'{PACKAGE}.assert_that', side_effect=AssertionError('message'))
-def test_match_when_assertion_fails(assert_that, matcher):
-    verification = match(matcher, sentinel.actual)
+def test_match_when_assertion_fails(matcher, mocker):
+    assert_that = mocker.patch(f'{PKG}.assert_that')
+    assert_that.side_effect = AssertionError('message')
 
+    verification = match(matcher, sentinel.actual)
     assert verification.status == Status.UNSTABLE
     assert verification.message == 'message'
     matcher.to_hamcrest.assert_called_once_with()
     assert_that.assert_called_once_with(sentinel.actual, sentinel.hamcrest)
 
 
-@patch(f'{PACKAGE}.assert_that')
-def test_match_when_the_assertion_succeeds(assert_that, matcher):
-    verification = match(matcher, sentinel.actual)
+def test_match_when_the_assertion_succeeds(matcher, mocker):
+    assert_that = mocker.patch(f'{PKG}.assert_that')
 
+    verification = match(matcher, sentinel.actual)
     assert verification.status == Status.SUCCESS
     assert_that.assert_called_once_with(sentinel.actual, sentinel.hamcrest)
