8f9e4cac3214c922429479f468759029366192e7
ymoch <ymoch.dev@gmail.com>
2020-07-09 00:38:07 +0900

    Refactor.

--- tests/compilation/test_yaml_load.py
+++ tests/compilation/test_yaml_load.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from unittest.mock import NonCallableMock, Mock, sentinel
 
 from pytest import mark, raises
 
@@ -29,7 +30,7 @@ def test_resolvable_interface():
     ('{key: !argument []}', [NamedNode('key')]),
     ('{key: [!argument {}]}', [NamedNode('key'), IndexedNode(0)]),
 ))
-def test_given_invalid_content(content, expected_path):
+def test_load_given_invalid_content(content, expected_path):
     io = StringIO(content)
     with raises(CompilationError) as error_info:
         load(io)
@@ -40,6 +41,39 @@ def test_load_all_given_invalid_value():
     io = StringIO('!invalid')
     with raises(CompilationError):
         next(load_all(io))
+
+
+@mark.parametrize('path, expected_origin', [
+    ('scenario.yml', ''),
+    ('/scenario.yml', '/'),
+    ('/path/to/scenario.yml', '/path/to'),
+    ('./scenario.yml', '.'),
+    ('./path/to/scenario.yml', './path/to'),
+])
+def test_load_from_path(path, expected_origin, mocker):
+    open_mock_entering = NonCallableMock(
+        __enter__=Mock(return_value=sentinel.io),
+        __exit__=Mock(),
+    )
+    open_mock = mocker.patch('builtins.open', return_value=open_mock_entering)
+
+    load_mock = mocker.patch('preacher.compilation.yaml.load')
+    load_mock.return_value = sentinel.obj
+
+    actual = load_from_path(path)
+    assert actual is sentinel.obj
+
+    open_mock_entering.__enter__.assert_called_once()
+    open_mock_entering.__exit__.assert_called_once()
+    open_mock.assert_called_once_with(path)
+    load_mock.assert_called_once_with(sentinel.io, expected_origin)
+
+
+def test_load_from_path_not_found(mocker):
+    mocker.patch('builtins.open', side_effect=FileNotFoundError('message'))
+
+    with raises(CompilationError):
+        load_from_path('/////foo/bar/baz')
 
 
 def test_load_all_from_path(mocker):
@@ -54,13 +88,6 @@ def test_load_all_from_path(mocker):
         next(actual)
 
     open_mock.assert_called_once_with('path/to/foo.yaml')
-
-
-def test_load_from_path_not_found(mocker):
-    mocker.patch('builtins.open', side_effect=FileNotFoundError('message'))
-
-    with raises(CompilationError):
-        load_from_path('/////foo/bar/baz')
 
 
 def test_load_all_from_path_not_found(mocker):
