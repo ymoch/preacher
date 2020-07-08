f602db3624e6ba92457efda1d159ae15e4f7a561
ymoch <ymoch.dev@gmail.com>
2020-07-09 00:34:56 +0900

    Refactor.

--- tests/compilation/test_yaml_load.py
+++ tests/compilation/test_yaml_load.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timezone, timedelta
 from io import StringIO
 
 from pytest import mark, raises
@@ -12,7 +11,6 @@ from preacher.compilation.yaml import (
     load_all,
     load_all_from_path,
 )
-from preacher.core.interpretation import RelativeDatetime, ValueContext
 
 
 def test_resolvable_interface():
@@ -25,67 +23,17 @@ def test_resolvable_interface():
         resolvable.resolve('')
 
 
-@mark.parametrize('content, expected_message, expected_path', (
-    ('!invalid foo', '!invalid', []),
-    ('- !include {}', '', [IndexedNode(0)]),
-    ('{key: !include {}}', '', [NamedNode('key')]),
-    ('{key: [!include {}]}', '', [NamedNode('key'), IndexedNode(0)]),
+@mark.parametrize(('content', 'expected_path'), (
+    ('!invalid', []),
+    ('- !argument {}', [IndexedNode(0)]),
+    ('{key: !argument []}', [NamedNode('key')]),
+    ('{key: [!argument {}]}', [NamedNode('key'), IndexedNode(0)]),
 ))
-def test_given_invalid_content(content, expected_message, expected_path):
+def test_given_invalid_content(content, expected_path):
     io = StringIO(content)
     with raises(CompilationError) as error_info:
         load(io)
-    assert expected_message in str(error_info.value)
     assert error_info.value.path == expected_path
-
-
-@mark.parametrize('content', [
-    '!relative_datetime []',
-    '!relative_datetime {}',
-    '!relative_datetime invalid',
-])
-def test_given_invalid_relative_datetime(content):
-    io = StringIO(content)
-    with raises(CompilationError):
-        load(io)
-
-
-def test_given_valid_relative_datetime():
-    io = StringIO('!relative_datetime -1 hour')
-    actual = load(io)
-    assert isinstance(actual, RelativeDatetime)
-
-    now = datetime.now()
-    resolved = actual.resolve(ValueContext(origin_datetime=now))
-    assert resolved.value == now - timedelta(hours=1)
-
-
-def test_given_valid_full_relative_datetime():
-    io = StringIO('!relative_datetime {delta: -1 minute, format: "%H:%M:%S"}')
-    actual = load(io)
-    assert isinstance(actual, RelativeDatetime)
-
-    now = datetime(2020, 1, 23, 12, 34, 56)
-    resolved = actual.resolve(ValueContext(origin_datetime=now))
-    assert resolved.formatted == '12:33:56'
-
-
-def test_given_datetime_that_is_offset_naive():
-    io = StringIO('2020-04-01 01:23:45')
-    actual = load(io)
-    assert isinstance(actual, datetime)
-    assert actual == datetime(2020, 4, 1, 1, 23, 45)
-    assert actual.tzinfo is None
-
-
-def test_given_datetime_that_is_offset_aware():
-    io = StringIO('2020-04-01 01:23:45 +09:00')
-    actual = load(io)
-    assert isinstance(actual, datetime)
-    assert (
-        actual - datetime(2020, 3, 31, 16, 23, 45, tzinfo=timezone.utc)
-    ).total_seconds() == 0.0
-    assert actual.tzinfo
 
 
 def test_load_all_given_invalid_value():
