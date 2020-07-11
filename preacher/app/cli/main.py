d82977116cfefadf8114156e868490b99d0f9f7c
ymoch <ymoch.dev@gmail.com>
2020-07-11 17:14:41 +0900

    Log errors.

--- preacher/app/cli/main.py
+++ preacher/app/cli/main.py
@@ -14,7 +14,7 @@ from preacher.compilation import (
 from preacher.core.listener import MergingListener
 from preacher.core.runner import ScenarioRunner
 from preacher.presentation.listener import LoggingListener, ReportingListener
-from preacher.yaml import load_all, load_all_from_path
+from preacher.yaml import YamlError, load_all, load_all_from_path
 from .log import ColoredFormatter
 from .option import parse_args
 
@@ -25,7 +25,7 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(HANDLER)
 
 
-def _main() -> None:
+def _main() -> bool:
     """Main."""
     args = parse_args(environ=os.environ)
 
@@ -61,13 +61,18 @@ def _main() -> None:
     with ThreadPoolExecutor(args.concurrency) as executor:
         status = app.run(executor, scenarios, listener=listener)
 
-    if not status.is_succeeded:
-        sys.exit(1)
+    return status.is_succeeded
 
 
 def main():
     try:
-        _main()
+        succeeded = _main()
+        if not succeeded:
+            sys.exit(1)
+    except YamlError as error:
+        LOGGER.critical('YAML loading error')
+        LOGGER.critical(error)
+        sys.exit(2)
     except CompilationError as error:
         LOGGER.critical(
             'Compilation error on node: %s',
