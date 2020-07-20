f54a9a611354951878de77cee1d14e942a0943ca
ymoch <ymoch.dev@gmail.com>
2020-07-20 17:17:56 +0900

    Simplify

--- tests/presentation/test_html_reporter.py
+++ tests/presentation/test_html_reporter.py
@@ -11,15 +11,7 @@ from unittest.mock import NonCallableMock
 
 from pytest import fixture, mark
 
-from preacher.core.request import (
-    ExecutionReport,
-    PreparedRequest,
-    Response,
-    ResponseBody,
-)
-from preacher.core.scenario import ScenarioResult, CaseResult
-from preacher.core.status import Status, StatusedList
-from preacher.core.verification import Verification, ResponseVerification
+from preacher.core.request import ExecutionReport, Response, ResponseBody
 from preacher.presentation.html import HtmlReporter
 from . import FILLED_SCENARIO_RESULTS
 
@@ -70,75 +62,6 @@ def test_export_execution(path):
 @mark.parametrize('results', [
     [],
     FILLED_SCENARIO_RESULTS,
-    [
-        ScenarioResult(),
-        ScenarioResult(
-            label='Scenario 1',
-            status=Status.SUCCESS,
-            conditions=Verification(
-                status=Status.SUCCESS,
-                children=[
-                    Verification(),
-                    Verification(Status.SUCCESS, message='msg'),
-                ],
-            ),
-            cases=StatusedList([
-                CaseResult(),
-                CaseResult(
-                    label='Case 1',
-                    execution=ExecutionReport()
-                ),
-                CaseResult(
-                    label='Case 2',
-                    conditions=Verification(
-                        status=Status.SUCCESS,
-                        children=[
-                            Verification(),
-                            Verification(Status.SUCCESS, message='msg'),
-                        ],
-                    ),
-                    execution=ExecutionReport(
-                        status=Status.SUCCESS,
-                        request=PreparedRequest(
-                            method='GET',
-                            url='https://base-url.com/path?foo=bar',
-                            headers={'User-Agent': 'Preacher x.x.x'},
-                            body='spam=ham',
-                        ),
-                    ),
-                    response=ResponseVerification(
-                        response_id='response-id',
-                        status=Status.SUCCESS,
-                        status_code=Verification(
-                            status=Status.SUCCESS,
-                            children=[
-                                Verification(),
-                                Verification(Status.SUCCESS, message='msg'),
-                            ],
-                        ),
-                        headers=Verification(
-                            status=Status.SUCCESS,
-                            children=[
-                                Verification(),
-                                Verification(Status.SUCCESS, message='msg'),
-                            ],
-                        ),
-                        body=Verification(
-                            status=Status.SUCCESS,
-                            children=[
-                                Verification(),
-                                Verification(Status.SUCCESS, message="msg"),
-                            ]
-                        )
-                    ),
-                ),
-            ]),
-            subscenarios=StatusedList([
-                ScenarioResult(),
-                ScenarioResult(label='Subscenario 1'),
-            ])
-        ),
-    ]
 ])
 def test_export_results(path, results):
     reporter = HtmlReporter(path)
