3dfe60a21f8ee86df48c04ea533ab03fec07f47f
ymoch <ymoch.dev@gmail.com>
2020-07-22 00:33:22 +0900

    Refactor.

--- preacher/compilation/yaml/argument.py
+++ preacher/compilation/yaml/argument.py
@@ -1,12 +1,12 @@
 from yaml import BaseLoader, Node
 
-from preacher.compilation.argument import ArgumentValue
+from preacher.compilation.argument import Argument
 from preacher.compilation.util.type import ensure_str
 from .error import on_node
 
 
-def construct_argument(loader: BaseLoader, node: Node) -> ArgumentValue:
+def construct_argument(loader: BaseLoader, node: Node) -> Argument:
     obj = loader.construct_scalar(node)
     with on_node(node):
         key = ensure_str(obj)
-    return ArgumentValue(key)
+    return Argument(key)
