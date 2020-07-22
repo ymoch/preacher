385f4b960a10908c7cf6abdf2876f8b4f2393eac
ymoch <ymoch.dev@gmail.com>
2020-07-22 12:08:16 +0900

    Remove unused codes.

--- preacher/app/cli/main.py
+++ preacher/app/cli/main.py
@@ -5,7 +5,7 @@ import os
 import sys
 from concurrent.futures import Executor
 from itertools import chain
-from typing import Iterable, Iterator, Sequence, Callable
+from typing import Iterator, Sequence, Callable
 
 import click
 from click import IntRange
@@ -34,50 +34,9 @@ LOGGER = get_logger(__name__, logging.WARNING)
 REPORT_LOGGER_NAME = 'preacher-cli.report.logger'
 
 
-def _main() -> None:
-    args = parse_args(environ=os.environ)
-
-    runner = ScenarioRunner(
-        base_url=args.url,
-        retry=args.retry,
-        delay=args.delay,
-        timeout=args.timeout,
-    )
-
-    if args.scenario:
-        objs: Iterable[object] = chain.from_iterable(
-            load_all_from_path(path) for path in args.scenario
-        )
-    else:
-        objs = load_all(sys.stdin)
-
-    compiler = create_scenario_compiler()
-    scenarios = chain.from_iterable(
-        compiler.compile_flattening(obj, arguments=args.argument)
-        for obj in objs
-    )
-
-    listener = MergingListener()
-    listener.append(LoggingReportingListener.from_logger(
-        get_logger(REPORT_LOGGER_NAME, args.level)
-    ))
-    if args.report:
-        listener.append(HtmlReportingListener.from_path(args.report))
-
-    with args.concurrent_executor_factory(args.concurrency) as executor:
-        status = runner.run(executor, scenarios, listener=listener)
-
-    if not status.is_succeeded:
-        sys.exit(1)
-
-
 def main_legacy():
     """Main."""
-    try:
-        _main()
-    except Exception as error:
-        LOGGER.exception('%s', error)
-        sys.exit(2)
+    parse_args(environ=os.environ)
 
 
 _ENV_PREFIX = 'PREACHER_CLI_'
