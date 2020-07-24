caad998eee6cf22bda4446492d14f2140cfee098
ymoch <ymoch.dev@gmail.com>
2020-07-24 11:42:38 +0900

    Refactor.

--- tests/compilation/verification/test_predicate_compiler.py
+++ tests/compilation/verification/test_predicate_compiler.py
@@ -6,13 +6,13 @@ PKG = 'preacher.compilation.verification.predicate'
 
 
 def test_matcher_predicate(mocker):
-    compile_hamcrest_factory = mocker.patch(f'{PKG}.compile_matcher_factory')
-    compile_hamcrest_factory.return_value = sentinel.factory
+    compile_matcher_factory = mocker.patch(f'{PKG}.compile_matcher_factory')
+    compile_matcher_factory.return_value = sentinel.factory
     predicate_ctor = mocker.patch(f'{PKG}.MatcherWrappingPredicate')
     predicate_ctor.return_value = sentinel.predicate
 
     predicate = PredicateCompiler().compile(sentinel.obj)
     assert predicate == sentinel.predicate
 
-    compile_hamcrest_factory.assert_called_once_with(sentinel.obj)
+    compile_matcher_factory.assert_called_once_with(sentinel.obj)
     predicate_ctor.assert_called_once_with(sentinel.factory)
