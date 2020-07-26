4602a2ad89896ea84ca92edada3497ce3d2cc05f
ymoch <ymoch.dev@gmail.com>
2020-07-26 15:53:46 +0900

    Set a dummy version.

--- examples/plugin/setup.py
+++ examples/plugin/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='preacher-plugin-example',
+    version='0.0.0',
     py_modules=['plugin_example'],
     entry_points={'preacher': '.preacher = plugin_example'},
     install_requires=['preacher'],
