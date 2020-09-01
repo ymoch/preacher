a43509e7a12618d3a94accd15e78c6b4241aaf79
ymoch <ymoch.dev@gmail.com>
2020-09-02 03:05:35 +0900

    Add tests.

--- tests/core/unit/test_unit_runner.py
+++ tests/core/unit/test_unit_runner.py
@@ -5,6 +5,7 @@ from pytest import mark, raises
 from preacher.core.request import ExecutionReport, Request
 from preacher.core.status import Status
 from preacher.core.unit.runner import predicate, UnitRunner
+from preacher.core.value import ValueContext
 from preacher.core.verification import ResponseVerification, ResponseDescription
 
 PKG = 'preacher.core.unit.runner'
@@ -60,3 +61,37 @@ def test_given_no_response(mocker):
     request.execute.assert_called_once_with(sentinel.base_url, timeout=None, session=None)
     requirements.verify.assert_not_called()
     retry.assert_called_once_with(ANY, attempts=1, delay=0.1, predicate=predicate)
+
+
+def test_given_a_response(mocker):
+    retry = mocker.patch(f'{PKG}.retry_while_false', side_effect=_retry)
+
+    execution = ExecutionReport(starts=sentinel.starts)
+    request = NonCallableMock(Request)
+    request.execute.return_value = (execution, sentinel.response)
+    requirements = NonCallableMock(ResponseDescription)
+    requirements.verify.return_value = sentinel.verification
+
+    runner = UnitRunner(
+        base_url=sentinel.base_url,
+        retry=3,
+        delay=sentinel.delay,
+        timeout=sentinel.timeout,
+        session=sentinel.session,
+    )
+    execution, response, verification = runner.execute(request, requirements)
+
+    assert execution is execution
+    assert response is sentinel.response
+    assert verification is sentinel.verification
+
+    request.execute.assert_called_with(
+        sentinel.base_url,
+        timeout=sentinel.timeout,
+        session=sentinel.session,
+    )
+    requirements.verify.assert_called_with(
+        sentinel.response,
+        ValueContext(origin_datetime=sentinel.starts),
+    )
+    retry.assert_called_once_with(ANY, attempts=4, delay=sentinel.delay, predicate=ANY)
