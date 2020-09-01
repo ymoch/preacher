b67199a47d9f114ac08077492351ba7c9657ed9f
ymoch <ymoch.dev@gmail.com>
2020-09-02 02:07:36 +0900

    Extend retrying.

--- preacher/core/scenario/util/retry.py
+++ preacher/core/scenario/util/retry.py
@@ -10,13 +10,18 @@ from typing import Callable, TypeVar
 T = TypeVar('T')
 
 
-def retry_while_false(func: Callable, attempts: int = 1, delay: float = 0.1):
+def retry_while_false(
+    func: Callable[[], T],
+    attempts: int = 1,
+    delay: float = 0.1,
+    predicate: Callable[[T], bool] = bool,
+):
     if attempts < 1:
         raise ValueError(f'`attempts` must be positive, given {attempts}')
 
     for _ in range(attempts - 1):
         result = func()
-        if result:
+        if predicate(result):
             return result
         time.sleep(delay)
 
