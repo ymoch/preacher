8b1cdbbf3f5285886efa0c42b761d3939740f351
ymoch <ymoch.dev@gmail.com>
2020-09-02 03:00:25 +0900

    Add tests.

--- tests/core/unit/test_unit_runner.py
+++ tests/core/unit/test_unit_runner.py
@@ -1,11 +1,17 @@
-from unittest.mock import NonCallableMock, sentinel
+from unittest.mock import ANY, NonCallableMock, sentinel
 
 from pytest import mark, raises
 
-from preacher.core.request import ExecutionReport
+from preacher.core.request import ExecutionReport, Request
 from preacher.core.status import Status
 from preacher.core.unit.runner import predicate, UnitRunner
-from preacher.core.verification import ResponseVerification
+from preacher.core.verification import ResponseVerification, ResponseDescription
+
+PKG = 'preacher.core.unit.runner'
+
+
+def _retry(func, *_args, **_kwargs):
+    return func()
 
 
 @mark.parametrize(('execution', 'verification', 'expected'), [
@@ -35,3 +41,22 @@ def test_predicate(execution, verification, expected):
 def test_given_invalid_retry_count(retry):
     with raises(ValueError):
         UnitRunner(sentinel.base_url, retry=retry)
+
+
+def test_given_no_response(mocker):
+    retry = mocker.patch(f'{PKG}.retry_while_false', side_effect=_retry)
+
+    request = NonCallableMock(Request)
+    request.execute.return_value = (sentinel.execution, None)
+    requirements = NonCallableMock(ResponseDescription)
+
+    runner = UnitRunner(sentinel.base_url)
+    execution, response, verification = runner.execute(request, requirements)
+
+    assert execution is sentinel.execution
+    assert response is None
+    assert verification is None
+
+    request.execute.assert_called_once_with(sentinel.base_url, timeout=None, session=None)
+    requirements.verify.assert_not_called()
+    retry.assert_called_once_with(ANY, attempts=1, delay=0.1, predicate=predicate)
