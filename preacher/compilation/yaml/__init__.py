e0b4a8cec88a2c829f1868096b4d2d65672f88b8
ymoch <ymoch.dev@gmail.com>
2021-06-10 00:42:43 +0900

    Simplify plugin dependency.

--- preacher/compilation/yaml/__init__.py
+++ preacher/compilation/yaml/__init__.py
@@ -4,5 +4,6 @@ YAML handling.
 
 from .error import YamlError
 from .loader import Loader
+from .factory import create_yaml_loader
 
-__all__ = ["Loader", "YamlError"]
+__all__ = ["Loader", "YamlError", "create_yaml_loader"]
