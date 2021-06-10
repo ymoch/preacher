00d8295ef62121146cb259293a2477cbd98a63e1
ymoch <ymoch.dev@gmail.com>
2021-06-11 01:07:14 +0900

    Log plugin manager existence.

--- preacher/compilation/extraction/factory.py
+++ preacher/compilation/extraction/factory.py
@@ -1,14 +1,21 @@
+from logging import Logger
 from typing import Optional
 
 from pluggy import PluginManager
 
 from preacher.compilation.extraction import ExtractionCompiler
+from preacher.core.logger import default_logger
 
 
 def create_extraction_compiler(
     plugin_manager: Optional[PluginManager] = None,
+    logger: Optional[Logger] = None,
 ) -> ExtractionCompiler:
+    logger = logger or default_logger
     compiler = ExtractionCompiler()
     if plugin_manager:
         plugin_manager.hook.preacher_add_extractions(compiler=compiler)
+    else:
+        message = "No plugin manager is given. The default extraction methods are not available."
+        logger.info(message)
     return compiler
