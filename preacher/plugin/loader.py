2e08d2cf32233acbde125f3d1cc173f68d2cdb40
ymoch <ymoch.dev@gmail.com>
2021-06-26 13:40:33 +0900

    Reformat with Black.

--- preacher/plugin/loader.py
+++ preacher/plugin/loader.py
@@ -9,7 +9,7 @@ from pluggy import PluginManager
 
 from preacher.core.logger import default_logger
 
-__all__ = ['load_plugins']
+__all__ = ["load_plugins"]
 
 
 def load_plugins(
@@ -39,7 +39,7 @@ def _load_module(path: str, logger: Logger) -> types.ModuleType:
 
     spec = spec_from_file_location(name, path)
     if not spec:
-        raise RuntimeError(f'Could not load as a module: {path}')
+        raise RuntimeError(f"Could not load as a module: {path}")
     module = module_from_spec(spec)
     spec.loader.exec_module(module)  # type: ignore
     sys.modules[name] = module  # To enable sub-processes use this module.
--- preacher/plugin/manager.py
+++ preacher/plugin/manager.py
@@ -4,18 +4,19 @@ from pluggy import PluginManager
 
 from . import hookspec
 
-__all__ = ['get_plugin_manager']
+__all__ = ["get_plugin_manager"]
 
 
 def get_plugin_manager() -> PluginManager:
     """
     Get a plugin manager that the entry points and the default implementation are already loaded.
     """
-    manager = PluginManager('preacher')
+    manager = PluginManager("preacher")
     manager.add_hookspecs(hookspec)
-    manager.load_setuptools_entrypoints('preacher')
+    manager.load_setuptools_entrypoints("preacher")
 
     from . import impl
+
     manager.register(impl)
 
     return manager
