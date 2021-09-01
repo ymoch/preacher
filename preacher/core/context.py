858e5015b40c6cc40f3b5e857ed90ab352dac017
ymoch <ymoch.dev@gmail.com>
2021-09-02 02:51:18 +0900

    Use closed context.

--- preacher/core/context.py
+++ preacher/core/context.py
@@ -10,7 +10,7 @@ CONTEXT_KEY_BASE_URL = "base_url"
 
 
 @contextmanager
-def closed_context(context: Context, **kwargs) -> Iterator:
+def closed_context(context: Context, **kwargs) -> Iterator[Context]:
     stored = {key: context[key] for key in kwargs if key in context}
 
     context.update(kwargs)
